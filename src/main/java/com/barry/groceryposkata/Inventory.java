fdac3f08920f0d9667e332a069d398e481b7c969
Barry Tarlton <btarlton@gmail.com>
2018-07-16 23:05:00 -0400

    add id generator for inventory items

--- src/main/java/com/barry/groceryposkata/Inventory.java
+++ src/main/java/com/barry/groceryposkata/Inventory.java
@@ -5,17 +5,24 @@ import java.util.List;
 
 public class Inventory {
 
+    private int greatestId = 0;
+
     List<Item> itemList = new ArrayList<Item>();
 
     public int addItem(String itemName, double itemPrice){
 
-        Item newItem = new Item(1);
+        Item newItem = new Item(generateNextId());
         newItem.setPrice(itemPrice);
 
         itemList.add(newItem);
 
         return newItem.getID();
 
+    }
+
+
+    private int generateNextId(){
+        return ++greatestId;
     }
 
 
--- src/test/java/com/barry/groceryposkata/InventoryTest.java
+++ src/test/java/com/barry/groceryposkata/InventoryTest.java
@@ -1,20 +1,34 @@
 package com.barry.groceryposkata;
 
+import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
-import org.junit.Test;
+
 
 
 public class InventoryTest {
 
-    @Test
-    public void addInventoryItem_addingNewItem_returnsGeneratedIdAsPositiveInt(){
+    private Inventory inventory;
 
-        Inventory inventory = new Inventory();
+    @Before
+    public void initializeInventory(){
+        this.inventory = new Inventory();
+    }
+
+    @Test
+    public void addItem_addingNewItem_returnsGeneratedIdAsPositiveInt(){
 
         int itemId = inventory.addItem("Twinkies", 3.00);
         assertTrue("adding item to inventory returns positive id",  itemId > 0);
+    }
+
+
+    @Test
+    public void addItem_addingTwoItems_generatesUniqueIdForEach(){
+
+        int itemId1 = inventory.addItem("Twinkies", 3.00);
+        int itemId2 = inventory.addItem("Swiss Cake Rolls", 2.75);
+        assertNotEquals(itemId1, itemId2);
     }
 
 }
