7d73f6fc962b3330c4a967616040af8aa8a698d5
Barry Tarlton <btarlton@gmail.com>
2018-07-17 15:51:53 -0400

    cleaned up tests

--- src/test/java/com/barry/groceryposkata/InventoryControllerTests.java
+++ src/test/java/com/barry/groceryposkata/InventoryControllerTests.java
@@ -1,17 +1,14 @@
 package com.barry.groceryposkata;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 
 import static org.hamcrest.CoreMatchers.is;
 
@@ -24,30 +21,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest()
-// @AutoConfigureMockMvc
+@AutoConfigureMockMvc
 public class InventoryControllerTests {
 
-	@InjectMocks
-	private InventoryController inventoryController;
-
-
+	@Autowired
 	private MockMvc mockMvc;
 
-	@Before
-	public void init(){
-		MockitoAnnotations.initMocks(this);
-		mockMvc = MockMvcBuilders.standaloneSetup(inventoryController).build();
-	}
-
+	@Test
 	public void addItem_whenAddingNewItem_returnsItemId() throws Exception {
 		mockMvc.perform(post("/inventory/items")
-				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON)
 				.content("{\"price\":\"2.50\"}"))
-				.andExpect(status().isOk());
-
-		//		.andExpect(content()
-		//				.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-		//		.andExpect(jsonPath("id", is("1")));
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("id", is("1")));
 	}
 
 
@@ -57,11 +44,9 @@ public class InventoryControllerTests {
 				.contentType(MediaType.APPLICATION_JSON_VALUE)
 				.accept(MediaType.APPLICATION_JSON))
 				.andDo(print())
-				.andExpect(status().isOk());
-
-		//		.andExpect(content()
-		//				.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-		//		.andExpect(jsonPath("id", is("1")));
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$[0].price", is(3)));
 	}
 
 }
