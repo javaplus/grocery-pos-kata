b1098d3684a2bb71c372fe106e587c0b661c6886
Barry Tarlton <btarlton@gmail.com>
2018-07-19 15:33:38 -0400

    remove item from cart functionality added. Removes all of that item though

--- src/main/java/com/barry/groceryposkata/controller/ShoppingCartController.java
+++ src/main/java/com/barry/groceryposkata/controller/ShoppingCartController.java
@@ -3,6 +3,7 @@ package com.barry.groceryposkata.controller;
 
 import com.barry.groceryposkata.entities.Item;
 import com.barry.groceryposkata.entities.ShoppingCartAddRequest;
+import com.barry.groceryposkata.service.Inventory;
 import com.barry.groceryposkata.service.ShoppingCart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,9 @@ public class ShoppingCartController {
 
     @Autowired
     private ShoppingCart shoppingCart;
+
+    @Autowired
+    private Inventory inventory;
 
 
     @RequestMapping(value = "/items", method = RequestMethod.POST)
@@ -32,6 +36,16 @@ public class ShoppingCartController {
         double total = shoppingCart.getItemTotal();
         String response = String.format("{\"total\":%s}",total);
         return response;
+
+    }
+
+
+    @RequestMapping(value = "/items/{itemName}", method = RequestMethod.DELETE)
+    public void removeItem(@PathVariable String itemName) throws Exception{
+
+        int itemID = inventory.getItemByName(itemName).getID();
+
+        shoppingCart.removeOrderedItem(itemID);
 
     }
 
--- src/test/java/com/barry/groceryposkata/controller/ShoppingCartControllerTests.java
+++ src/test/java/com/barry/groceryposkata/controller/ShoppingCartControllerTests.java
@@ -21,6 +21,7 @@ import java.util.HashMap;
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.Assert.*;
@@ -139,6 +140,30 @@ public class ShoppingCartControllerTests {
 
 
     @Test
+    public void addItem_whenAdding2ofSameItem_cartTotalReflectsDoubleItemPrice() throws Exception{
+
+        // add item to inventory
+        String itemName = "Gobstoppers";
+        inventory.addItem(itemName,2.50);
+
+        String jsonRequest = String.format("{\"itemName\":\"%s\"}", itemName);
+
+        mockMvc.perform(post("/shoppingcart/items")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(post("/shoppingcart/items")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest))
+                .andExpect(status().isOk());
+
+
+        // verify shopping cart total is appropriate
+        assertEquals(5.00, shoppingCart.getItemTotal(), 0.001);
+    }
+
+    @Test
     public void getTotal_whenNothingInShoppingCart_returnsZeroTotal() throws Exception{
 
         // nothing added to cart...
@@ -186,6 +211,34 @@ public class ShoppingCartControllerTests {
         mockMvc.perform(get("/shoppingcart/total"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.total", is(expectedPrice.doubleValue())));
+
+
+    }
+
+
+
+    @Test
+    public void removeItem_removingItemFromCart_reducesTotalByItemPrice() throws Exception{
+
+        String itemName1 = "KitKat";
+        double price1 = 10.33;
+        inventory.addItem(itemName1,price1);
+
+        String itemName2 = "Watchmacallit";
+        double price2 = 2.55;
+        inventory.addItem(itemName2,2.55);
+
+        // add our item directly to the shopping cart
+        shoppingCart.addItem(itemName1);
+        shoppingCart.addItem(itemName2);
+
+        // expectedPrice is sum of two prices.
+        BigDecimal expectedPrice = new BigDecimal(price1 + price2).setScale(2, RoundingMode.HALF_UP);
+
+        mockMvc.perform(delete("/shoppingcart/items/KitKat"))
+                .andExpect(status().isOk());
+
+        assertEquals(2.55, shoppingCart.getItemTotal(), 0.001);
 
 
     }
